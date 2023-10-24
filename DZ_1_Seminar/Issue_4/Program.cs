5d219d02b311acfa41d7b33222df821eca3d43a1
Maksim Burda <maksimburda1@gmail.com>
2023-10-24 18:53:20 +0300

    Issue_4 done

--- DZ_1_Seminar/Issue_4/Program.cs
+++ DZ_1_Seminar/Issue_4/Program.cs
@@ -1,5 +1,9 @@
-﻿// Напишите программу, которая на вход принимает число (N), 
+﻿// Напишите программу, которая на вход принимает число (N),
 // а на выходе показывает все чётные числа от 1 до N.
 Console.Clear();
 Console.Write("Введите число a = ");
 int a = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 2; i <= a; i = i + 2)
+    Console.Write(i + " ");
+    Console.WriteLine();
