07826b40149af8c8e6c5dab12b2b2e612402f5dd
Maksim Burda <maksimburda1@gmail.com>
2023-10-24 18:13:49 +0300

    Issue_2 is done

--- DZ_1_Seminar/Issue_2/Program.cs
+++ DZ_1_Seminar/Issue_2/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая
+//принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.Clear();
+Console.Write("Введите первое число a = ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число b = ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число с = ");
+int c = Convert.ToInt32(Console.ReadLine());
+int max;
+if (a > b)
+{
+    max = a;
+}
+else
+{
+    max = b;
+}
+if (max > c)
+{
+    Console.Write("Максимальное число ");
+    Console.WriteLine(max);
+}
+else
+{
+    max = c;
+    Console.Write("Максимальное число ");
+    Console.WriteLine(max);
+}
