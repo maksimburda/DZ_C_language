d073216216b59ee0f61ac3ba9d78f53b953e27fb
Maksim Burda <maksimburda1@gmail.com>
2023-10-24 18:26:29 +0300

    Issue_3 done

---
+++ DZ_1_Seminar/Issue_3/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая на вход принимает число и выдаёт
+//, является ли число чётным (делится ли оно на два без остатка).
+Console.Clear();
+Console.Write("Введите число a = ");
+int a = Convert.ToInt32(Console.ReadLine());
+if(a%2==0)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
