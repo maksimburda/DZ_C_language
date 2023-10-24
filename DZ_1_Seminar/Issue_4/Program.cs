f91516b6992d5c99f7ed8a0f98067540ebc496c0
Maksim Burda <maksimburda1@gmail.com>
2023-10-24 18:30:24 +0300

    issue_4 changed

--- DZ_1_Seminar/Issue_4/Program.cs
+++ DZ_1_Seminar/Issue_4/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+Console.Clear();
+Console.Write("Введите число a = ");
+int a = Convert.ToInt32(Console.ReadLine());
